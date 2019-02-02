bca974380bd1b06c9e058c6af3196b854f1c81e0
Ahmet Gurkan Koyluoglu <akoyluoglu@etu.edu.tr>
2019-02-02 12:11:10 +0300

    Getting ready for deployment to heroku

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,4 +1,14 @@
 package com.mycompany.app;
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
 
 /**
  * Hello world!
@@ -22,4 +32,50 @@ public class App
 	}
 	return 0;
     }
+public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+
+        get("/", (req, res) -> "Hello, World");
+
+        post("/compute", (req, res) -> {
+          //System.out.println(req.queryParams("input1"));
+          //System.out.println(req.queryParams("input2"));
+
+          String input1 = req.queryParams("input1");
+          java.util.Scanner sc1 = new java.util.Scanner(input1);
+          sc1.useDelimiter("[;\r\n]+");
+          int inputList[] = new int[10];
+          int a=0;
+          while (sc1.hasNext())
+          {
+            int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+            inputList[a]=value;
+            a++;
+          }
+          System.out.println(inputList);
+          int result = App.app(inputList, inputList , 1 , 2 , '+');
+
+         Map map = new HashMap();
+          map.put("result", result);
+          return new ModelAndView(map, "compute.mustache");
+        }, new MustacheTemplateEngine());
+
+
+        get("/compute",
+            (rq, rs) -> {
+              Map map = new HashMap();
+              map.put("result", "not computed yet!");
+              return new ModelAndView(map, "compute.mustache");
+            },
+            new MustacheTemplateEngine());
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+     return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
 }
+
